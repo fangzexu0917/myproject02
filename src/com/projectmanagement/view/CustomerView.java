34c4bbcf26a813343b8ca6626d2cdc93c486bcee
fangzexu0917 <870535620@qq.com>
2021-09-26 23:06:30 +0800

    V1.0

--- src/com/projectmanagement/view/CustomerView.java
+++ src/com/projectmanagement/view/CustomerView.java
@@ -11,7 +11,7 @@ public class CustomerView {
     private CustomerList customerList = new CustomerList(10);
 
     public CustomerView() {
-        Customer customer = new Customer("方泽旭",'男',23,"18768080956","870535620@qq.com");
+        Customer customer = new Customer("方泽旭", '男', 23, "18768080956", "870535620@qq.com");
         customerList.addCustomer(customer);
     }
 
@@ -67,9 +67,9 @@ public class CustomerView {
         String email = CMUtility.readString(40);
         Customer customer = new Customer(name, gender, age, tel, email);
         boolean isSuccess = customerList.addCustomer(customer);
-        if(isSuccess){
+        if (isSuccess) {
             System.out.println("---------------------添加完成---------------------");
-        }else{
+        } else {
             System.out.println("------------------客户目录已满，添加失败------------------");
         }
     }
@@ -78,14 +78,78 @@ public class CustomerView {
      * 修改客户
      */
     private void modifyCustomer() {
+        System.out.println("---------------------修改客户---------------------");
+        Customer cust;
+        int index;
+        do {
+            System.out.println("  请输入待修改客户编号（-1退出）：");
+            index = CMUtility.readInt();
+            if (index == -1) {
+                return;
+            }
+            cust = customerList.getCustomer(index - 1);
+            if (cust == null) {
+                System.out.println("  无法找到指定客户！：");
+            } else {
+                break;
+            }
+        } while (true);
+        System.out.print("姓名(" + cust.getName() + ")：");
+        String name = CMUtility.readString(4, cust.getName());
 
+        System.out.print("性别(" + cust.getGender() + ")：");
+        char gender = CMUtility.readChar(cust.getGender());
+
+        System.out.print("年龄(" + cust.getAge() + ")：");
+        int age = CMUtility.readInt(cust.getAge());
+
+        System.out.print("电话(" + cust.getPhone() + ")：");
+        String phone = CMUtility.readString(15, cust.getPhone());
+
+        System.out.print("邮箱(" + cust.getEmail() + ")：");
+        String email = CMUtility.readString(15, cust.getEmail());
+
+        cust = new Customer(name, gender, age, phone, email);
+
+        boolean isModified = customerList.replaceCustomer(index - 1, cust);
+        if (isModified) {
+            System.out
+                    .println("---------------------修改完成---------------------");
+        } else {
+            System.out.println("----------无法找到指定客户,修改失败--------------");
+        }
     }
+
 
     /**
      * 删除客户
      */
     private void deleteCustomer() {
-
+        System.out.println("---------------------删除客户---------------------");
+        int index;
+        Customer cust;
+        while (true) {
+            System.out.println("  请输入待删除客户编号（-1退出）：");
+            index = CMUtility.readInt();
+            if (index == -1)
+                return;
+            cust = customerList.getCustomer(index - 1);
+            if (cust == null) {
+                System.out.println("  无法找到指定客户！：");
+            } else {
+                break;
+            }
+        }
+        System.out.printf("确认是否删除(Y/N): ");
+        char delete = CMUtility.readConfirmSelection();
+        if (delete == 'Y') {
+            boolean isDeleted = customerList.deleteCustomer(index - 1);
+            if (isDeleted) {
+                System.out.println("---------------------删除完成---------------------");
+            } else {
+                System.out.println("----------无法找到指定客户,删除失败--------------");
+            }
+        }
     }
 
     /**
@@ -101,7 +165,7 @@ public class CustomerView {
             System.out.println("编号\t姓名\t\t性别\t\t年龄\t\t电话\t\t邮箱");
             for (int i = 0; i < custs.length; i++) {
                 Customer cust = custs[i];
-                System.out.println((i + 1) +"\t"+ cust.info());
+                System.out.println((i + 1) + "\t" + cust.info());
             }
         }
         System.out.println("----------------客户列表完成---------------");
